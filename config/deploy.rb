78ef72d6594a1cce8e5cb527e47275671733810f
Jared Jenkins <jay,sqrd@gmail.com>
2013-07-26 08:57:47 -0700

    Adding capistrano deployment to the project. Able to deploy, but need to look into asset bundling issues.

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+require "bundler/capistrano" 
+
+set :application, "pitch"
+set :repository,  "git@github.com:Undclrd/pitch.git"
+set :deploy_to, "/var/www/pitch"
+
+#deployment server's user
+set :user, "ubuntu"
+set :use_sudo, true
+
+#github settings
+set :scm, :git
+set :ssh_options, { :forward_agent => true }
+set :branch, "master"
+
+role :web, "pitch-web" # Your HTTP server, Apache/etc
+role :app, "pitch-web" # This may be the same as your `Web` server
+
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end

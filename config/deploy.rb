a5165c7959f8df7c9770c8cc77e8498fbcadb3d6
Jared Jenkins <jay,sqrd@gmail.com>
2013-08-14 00:56:33 -0700

    Adding DB deployment to Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :branch, "master"
 role :web, "pitch-web" # Your HTTP server, Apache/etc
 role :app, "pitch-web" # This may be the same as your `Web` server
 
-#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "pitch-web", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
@@ -24,9 +24,18 @@ role :app, "pitch-web" # This may be the same as your `Web` server
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
+namespace :db do
+  task :db_config, :except => { :no_release => true }, :role => :app do
+    run "cp -f ~/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after "deploy:finalize_update", "db:db_config"
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
+  
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end

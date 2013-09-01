eaa925787e0a0bd9852e2103581564ed9da76efc
Jared Jenkins <jay,sqrd@gmail.com>
2013-09-01 11:47:35 -0700

    Adding basic controller logic for cards.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def render_404
+    render { :text => "Can't seem to find what you're looking for. Are you sure this exists?", :status => 404 }
+  end
+
+  def render_403
+    render { :text => "You don't have permission to do this.", :status => 403 }
+  end
 end
--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,12 +1,19 @@
 class CardController < ApplicationController
+  before_filter :authenticate_user!
 
   def index
+    all_cards = Card.all
+    render :json => { :status => 'success',
+      :result =>  { :data => all_cards}
+    }
   end
 
-  def view
+  def show
+    #for viewing the specific details of a card.
   end
 
   def create
+    #POST to create a card
 
   end
 
@@ -14,7 +21,22 @@ class CardController < ApplicationController
 
   end
 
-  def destroy
+  def update
+    #edit an old card, can only access this if you created this card
+    @card = Card.find(params[:id])
 
+    if @card.nil?
+      render_404
+      return
+    end
+    if current_user.id <> @card.user.id
+      render_403
+      return
+    end
+  end
+
+  def destroy
+    #can't destroy this card
+    @card = Card.find(params[:id])
   end
 end

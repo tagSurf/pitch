1fd743a14649a3cc174807e20f7ed8c75754705c
Jared Jenkins <jay,sqrd@gmail.com>
2013-09-11 00:23:12 -0700

    Require at least 10 characters for the card excerpt

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ActiveRecord::Base
   belongs_to :user, :foreign_key => :author_id, :primary_key => :id
 
-  validates :excerpt, length: { maximum: 140 }
+  validates :excerpt, length: { minimum: 10, maximum: 140 }
 end
